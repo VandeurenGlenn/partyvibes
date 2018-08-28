e87f84725a831ae77324bfb889181536c15eaa82
Glenn Vandeuren <vandeurenglenn@gmail.com>
2018-08-29 00:56:14 +0200

    :metal: tempo-slider: update
    Beautify & decent selection system

--- src/app/party-tempo-slider.js
+++ src/app/party-tempo-slider.js
@@ -4,6 +4,7 @@ import RenderMixin from '../../node_modules/custom-renderer-mixin/src/render-mix
 
 import './party-slider';
 import './party-button';
+import lines from './../utils/lines'
 
 export default define(class PartyTempoSlider extends RenderMixin(HTMLElement) {
 
@@ -18,29 +19,44 @@ export default define(class PartyTempoSlider extends RenderMixin(HTMLElement) {
   constructor() {
     super()
     this.attachShadow({mode: 'open'});
-    this.percentage = 4
-    // disable RenderMixin's first render
-    // this.rendered = true;
+    this.percentage = .4;
+    this._onSelected = this._onSelected.bind(this);
+
+
   }
 
   connectedCallback() {
     if (super.connectedCallback) super.connectedCallback();
-    this.selector.addEventListener('selected', this._onSelected);
+    this.render({lines: lines({count: [
+      {width: 2},
+      {width: 4},
+      {width: 6},
+      {width: 8},
+      {width: 10},
+      {width: 12},
+      {width: 14},
+      {width: 12},
+      {width: 10},
+      {width: 8},
+      {width: 6},
+      {width: 4},
+      {width: 2}
+    ]})})
+
+    this.shadowRoot.querySelector('custom-select').addEventListener('selected', this._onSelected);
     this.slider.addEventListener('input', ({ detail }) => {
-      // this.percentage
-      // 0.5 =1
-      detail = detail * 2
+      this.current = parseFloat(detail);
+      if (this.current === 0.5) detail = this.current * 2;
+      else  detail = (this.current - (this.current * (this.percentage / 10))) * 2;
+
       this.dispatchEvent(new CustomEvent('change', {detail}))
     });
-    // this.input.addEventListener('input', this.oninput);
   }
 
   _onSelected({detail}) {
-    this.percentage = detail
-  }
-
-  shouldRender() {
-    // if (this.value) this.render({value: this.value}, this.template)
+    this.percentage = parseFloat(detail);
+    detail = (this.current - (this.current * (this.percentage / 10))) * 2;
+    this.dispatchEvent(new CustomEvent('change', {detail}))
   }
 
   get template() {
@@ -50,6 +66,16 @@ export default define(class PartyTempoSlider extends RenderMixin(HTMLElement) {
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .column {
+    display: flex;
+    flex-direction: column;
   }
 
   .flex {
@@ -74,15 +100,29 @@ export default define(class PartyTempoSlider extends RenderMixin(HTMLElement) {
     height: 200px;
   }
 
+  .lines {
+    padding: 6px 0;
+  }
+
+  .lines.left {
+    align-items: flex-end;
+  }
+
 </style>
 <span class="flex2"></span>
-<party-slider name="bpm" min="0" max="3" step="0.01" vertical></party-slider>
+<span class="row">
+  <span class="column lines left">${'lines'}</span>
+
+  <party-slider name="bpm" min="0" max="3" step="0.01" vertical></party-slider>
+
+  <span class="column lines">${'lines'}</span>
+</span>
 <span class="flex"></span>
-<custom-select selected="4" attr-for-selected="data-id">
-  <span class="percentage row center" data-id="100">100</span>
-  <span class="percentage row center" data-id="16">16</span>
-  <span class="percentage row center" data-id="8">8</span>
-  <span class="percentage row center" data-id="4">4</span>
+<custom-select selected=".2" attr-for-selected="data-id">
+  <span class="percentage row center" data-id="1.6">16</span>
+  <span class="percentage row center" data-id=".8">8</span>
+  <span class="percentage row center" data-id=".4">4</span>
+  <span class="percentage row center" data-id=".2">2</span>
 </custom-select>
 <span class="flex2"></span>
 <party-button>lock (pitch)</party-button>
