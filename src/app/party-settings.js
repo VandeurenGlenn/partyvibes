6633a050ea7f2045755a5421441a31e51fc705e5
Glenn Vandeuren <vandeurenglenn@gmail.com>
2018-09-30 17:22:07 +0200

    :metal: Render collection paths

--- src/app/party-settings.js
+++ src/app/party-settings.js
@@ -9,13 +9,22 @@ export default define(class PartySettings extends RenderMixin(HTMLElement) {
     super();
     this.attachShadow({mode: 'open'})
 
-    this._addFolder = this._addFolder.bind(this)
+    this._addFolder = this._addFolder.bind(this);
+    this._back = this._back.bind(this);
   }
 
   connectedCallback() {
     import(`./party-settings-template.js`).then(async ({ template }) => {
       this.template = template;
+      this.properties = {paths: window.party.config.paths.map(path => `<span class="path">${path}</span>`)}
       if (super.connectedCallback) super.connectedCallback()
+
+      Object.defineProperty(this, 'paths', {
+        set: value => {
+          this.properties.paths = value.map(path => `<span class="path">${path}</span>`)
+          this.render(this.properties)
+        }
+      })
 
       const devices = await navigator.mediaDevices.enumerateDevices();
       const audioDevices = devices.filter(device => device.kind === 'audiooutput'); // && device.deviceId !== 'communications' );
@@ -25,9 +34,14 @@ export default define(class PartySettings extends RenderMixin(HTMLElement) {
       selects[0].items = audioDevices;
       selects[1].items = audioDevices
 
+      this.shadowRoot.querySelector('button[title="back"]').addEventListener('click', this._back);
       this.shadowRoot.querySelector('.add').addEventListener('click', this._addFolder);
       // console.log('Audio is being played on ' + audio.sinkId);
     });
+  }
+
+  _back() {
+    document.dispatchEvent(new CustomEvent('back'))
   }
 
   _addFolder() {
@@ -39,10 +53,8 @@ export default define(class PartySettings extends RenderMixin(HTMLElement) {
     input.setAttribute('webkitdirectory', '')
     input.addEventListener('change', e => {
       window.party.config.paths.push(input.files[0].path);
-      const writer = new Worker('workers/write.js')
-
-      writer.postMessage({path: '../../config.json', data: JSON.stringify(window.party.config)})
-
+      window.party.saveConfig();
+      this.paths = window.party.config.paths;
       window.watcher.postMessage({add: true, paths: input.files[0].path});
 
     })
